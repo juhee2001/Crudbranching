9da3d887298318dbca09b7e1dfe0484cd13c9646
Girish <Girish@LAPTOP-7IKR08SV>
2025-01-03 16:32:36 +0530

    add

--- CrudBranching/src/main/java/com/cjc/controller/HomeController.java
+++ CrudBranching/src/main/java/com/cjc/controller/HomeController.java
@@ -6,11 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cjc.model.Student;
 import com.cjc.service.HomeService;
 
-@Controller
+@RestController
 public class HomeController {
   
 	@Autowired 
