0c98da510f17675b86bed368bcf1dd25c8e60266
Harshada12T <thoratharshada065@gmail.com>
2025-01-03 15:11:22 +0530

    add post method in HomeController
    add method by Harshada

--- CrudBranching/src/main/java/com/cjc/controller/HomeController.java
+++ CrudBranching/src/main/java/com/cjc/controller/HomeController.java
@@ -4,5 +4,12 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class HomeController {
-
+@AutoWired 
+  HomeService hs;
+  @PostMapping("/addData)
+  public List<Student> addData(@RequestBody List<Student> list)
+  {
+  List<Student> al=hs.add(list);
+    return al;
+  }
 }
