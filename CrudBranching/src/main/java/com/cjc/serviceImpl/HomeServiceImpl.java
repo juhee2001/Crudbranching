ed1cfdd3cd38de8aacea80275e66e17077a91a5b
Girish <Girish@LAPTOP-7IKR08SV>
2025-01-03 15:06:16 +0530

    add Student Class data

---
+++ CrudBranching/src/main/java/com/cjc/serviceImpl/HomeServiceImpl.java
@@ -0,0 +1,10 @@
+package com.cjc.serviceImpl;
+
+import org.springframework.stereotype.Service;
+
+import com.cjc.service.HomeService;
+
+@Service
+public class HomeServiceImpl implements HomeService {
+
+}
