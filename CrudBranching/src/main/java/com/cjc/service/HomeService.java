79128f798fe11e86071a6c1b4debf644d6d20d9d
Girish <Girish@LAPTOP-7IKR08SV>
2025-01-03 15:20:26 +0530

    add post method in service layer and repository layer

--- CrudBranching/src/main/java/com/cjc/service/HomeService.java
+++ CrudBranching/src/main/java/com/cjc/service/HomeService.java
@@ -1,5 +1,11 @@
 package com.cjc.service;
 
+import java.util.List;
+
+import com.cjc.model.Student;
+
 public interface HomeService {
+
+	public List<Student> add(List<Student> list);
 
 }
--- CrudBranching/src/main/java/com/cjc/serviceImpl/HomeServiceImpl.java
+++ CrudBranching/src/main/java/com/cjc/serviceImpl/HomeServiceImpl.java
@@ -1,10 +1,24 @@
 package com.cjc.serviceImpl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cjc.model.Student;
+import com.cjc.repository.HomeRepository;
 import com.cjc.service.HomeService;
 
 @Service
 public class HomeServiceImpl implements HomeService {
+
+	@Autowired
+	HomeRepository hr;
+	
+	@Override
+	public List<Student> add(List<Student> list) {
+		List<Student> li=(List<Student>)hr.saveAll(list);
+		return li;
+	}
 
 }
